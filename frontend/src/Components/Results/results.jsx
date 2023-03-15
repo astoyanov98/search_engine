aa1d1a3e42312b791ba77df6d3b6738ae5ed5aeb
astoyanov98 <49924052+astoyanov98@users.noreply.github.com>
2023-03-15 15:24:35 -0700

    adding links

--- frontend/src/Components/Results/results.jsx
+++ frontend/src/Components/Results/results.jsx
@@ -10,6 +10,7 @@ const Results = ({data, timeElapsed}) => {
                     return(
                         <div key={el.id} className='row justify-content-center wrapper'>
                             <div className='col-12 col-lg-4'>
+                                <span style={{fontSize:'13px'}}>{el.link}</span>
                                 <h3 ><a href={el.link}>{el.title}</a></h3>
                                 <span>{el.description}</span>
                             </div>
